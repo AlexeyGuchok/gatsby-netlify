ddd5819894201413fff2d4f84110acdae75c3593
Alexey Guchok <lu4ok95@mail.ru>
2021-01-11 15:53:00 +0300

    accessability improvements

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react"
 import styles from "./styles.module.css"
 
 const Footer = () => (
-    <footer className={styles.footer}>
-        <div className="container">
-            <span className="text-muted">This is our footer container</span>
-        </div>
-    </footer>
+  <footer className={styles.footer}>
+    <div className="container">
+      <span className="text-dark">This is our footer container</span>
+    </div>
+  </footer>
 )
 
 export default Footer
